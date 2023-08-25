24dff67644d3a06c2096aea85eb3b596523ea3cb
pintuBeast <abhishek.jk1992@gmail.com>
2023-08-25 11:56:43 +0530

    q6 done

--- aj4119_hw7_q6.cpp
+++ aj4119_hw7_q6.cpp
@@ -1,11 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
-int arraySearch(int[], int, int );
+void arraySearch(int[], int, int );
 
-int arraySearch(int arr[], int arrSize,int element)
+void arraySearch(int arr[], int arrSize,int element)
 {
+    string indexList="";
+    int indexCount=0;
+    for(int i=0;i<arrSize;i++)
+    {
+        if(arr[i]==element )
+        {
+            if(indexCount==0)
+            {
+                indexList += to_string(i+1);
+            }
+            else
+            {
+                indexList+=", "+to_string(i+1);
+
+            }
+            indexCount++;
+        }
+    }
+    if(indexCount==0)
+    {
+        cout<<element<<" does not show in the sequence";
+    }
+    else
+    {
+        cout<<element<<" shows in line "<<indexList;
+    }
 
 }
 
@@ -57,10 +84,45 @@ int* createArray(string combinedString,int& numIntegers)
     return intArray;
 }
 
-int vectorSearch(vector<int> , int );
+void vectorSearch(vector<int> , int );
+void vectorSearch(vector<int> sequenceVector , int element)
+{
+    
+    
+    string indexList="";
+    int indexCount=0;
 
-int main()
-{   string combinedString="";
+    for(int i=0;i<sequenceVector.size();i++)
+    {
+        if(sequenceVector[i]==element)
+        {
+            if(indexCount==0)
+            {
+                indexList += to_string(i+1);
+            }
+            else
+            {
+                indexList+=", "+to_string(i+1);
+
+            }
+            indexCount++;
+        }
+    }
+
+    if(indexCount==0)
+    {
+        cout<<element<<" does not show in the sequence";
+    }
+    else
+    {
+        cout<<element<<" shows in line "<<indexList;
+    }
+
+}
+
+void main1()
+{  cout<<"\n\n#### METHOD-1 ####\n\n";
+    string combinedString="";
      string inputString="";
     cout<<"Please Enter a sequence of positive integers, each in a separate line.\nEnd your input by typing -1.\n";
     while(inputString[0]!='-')
@@ -71,22 +133,46 @@ int main()
             combinedString+=inputString+" ";
         }
     }
-    cout<<"\nThe input is :"<<combinedString;
+   // cout<<"\nThe input is :"<<combinedString;
     int numIntegers;
     int* arr= createArray(combinedString,numIntegers);
-    cout<<"The integers are\n";
-    for(int i=0;i<numIntegers;i++)
-    {
-        cout<<arr[i]<<"\n";
-    }
-
     int elementToSearch;
-
-    cout<<"Please enter a number you want to search:\n";
+    cout<<"\nPlease enter a number you want to search:\n";
     cin>>elementToSearch;
+    arraySearch(arr,numIntegers,elementToSearch);
 
+}
 
+void main2()
+{
+    cout<<"\n\n#### METHOD-2 ####\n\n";
+    vector<int> sequenceVector;
+    int input=0;
+    cout<<"Please Enter a sequence of positive integers, each in a separate line.\nEnd your input by typing -1.\n";
+    while(input!=-1)
+    {
+        cin>>input;
+        if(input != -1)
+        {
+            sequenceVector.push_back(input);
+        }
+    }
+    cout<<"The integers are\n";
+    for(int i=0;i<sequenceVector.size();i++)
+    {
+        cout<<sequenceVector[i]<<"\n";
 
+    }
+    int elementToSearch;
+    cout<<"\nPlease enter a number you want to search:\n";
+    cin>>elementToSearch;
+    vectorSearch(sequenceVector,elementToSearch);
 
+}
+int main()
+{  
+    //main1();
+    main2();
+     
     return 0;
 }
